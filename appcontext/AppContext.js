d01dcf944f26a5f8de78a4c56010da0eb8a32f49
Julian <informacionlinux271087@gmail.com>
2023-07-02 16:54:12 -0700

    Upgrade grettings en pantalla Dashboard

--- appcontext/AppContext.js
+++ appcontext/AppContext.js
@@ -63,22 +63,12 @@ export const ContextProvider = ({ children }) => {
       withCredentials: true
     })
     .then((res) => {
-      alert("usuario logueado")
       console.log(res.data)
       setCredentials(res.data)
       return res.status
     })
     .catch(error => {
-      if (error.response) {
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log('Error', error.message);
-      }
-      console.log(error.config);
+     console.log(error)
     });
   }
   
